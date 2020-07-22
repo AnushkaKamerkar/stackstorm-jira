fff12982541c905ada0ece0e9312f75a40bacc55
Anushka Kamerkar <anushka@coditation.com>
2020-07-22 19:13:08 +0530

    Initial commit

--- actions/get_users.py
+++ actions/get_users.py
@@ -6,9 +6,10 @@ __all__ = [
 
 
 class GetJiraUserAction(BaseJiraAction):
-	def run(self, group):
-		group = self._client.group_members("jira-users")
-		for users in group:
-		    print users
-
-		return group
+	def run(self, accountId):
+		url = "https://thrivecs.atlassian.net/rest/api/3/user/bulk"
+		auth = HTTPBasicAuth("anushka@coditation.com", "rBC647X9WzvSUY3JZV15152F")
+		headers = {"Accept": "application/json"}
+		query = {'accountId': '5b4f0eb54351c62c0572b071'}
+		response = requests.request("GET",url,headers=headers,params=query,auth=auth)
+		print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
