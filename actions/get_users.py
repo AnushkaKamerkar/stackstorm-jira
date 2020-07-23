328b6aa84c9e8e1192c294f1ba3efeeeb9ad9f37
Anushka Kamerkar <anushka@coditation.com>
2020-07-23 20:18:04 +0530

    Initial commit

--- actions/get_users.py
+++ actions/get_users.py
@@ -1,13 +1,25 @@
-from lib.base import BaseJiraAction
+from st2common.runners.base_action import Action
 from requests.auth import HTTPBasicAuth
+import requests
+from flatten_dict import flatten
 
 __all__ = [
     'GetJiraUserAction'
 ]
 
-class GetJiraUserAction(BaseJiraAction):
+class GetJiraUserAction(Action):
 	def run(self, accountId):
-		user = self.get_users(accountId)
-		return user
+		email=self.config['email']
+		token = self.config['api_token']
+		base_url = self.config['url']
+		
+		url = base_url + "/rest/api/3/user/bulk"
+		auth = HTTPBasicAuth(email, token)
+		headers = {"Accept": "application/json"}
+		query = {'accountId': accountId}
+		response = requests.request("GET",url,headers=headers,params=query,auth=auth)
+		result = flatten(response.json())
+		print(type(result))
+		return result
 
 		
