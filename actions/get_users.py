7f16c930a092179cf5edcd166a9b02157b840880
Anushka Kamerkar <anushka@coditation.com>
2020-07-23 11:29:39 +0530

    Initial commit

--- actions/get_users.py
+++ actions/get_users.py
@@ -7,7 +7,7 @@ __all__ = [
 
 class GetJiraUserAction(BaseJiraAction):
 	def run(self, accountId):
-		user = self._client.get_users(accountId)
+		user = self.get_users(accountId)
 		return user
 
 		
